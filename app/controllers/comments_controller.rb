7a5dde00ed49077e02dc6cfec06b1a3e48ca97e8
keilsmit <keilsmit@yahoo.com>
2016-11-01 23:26:09 -0400

    cant leave a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,28 @@
 class CommentsController < ApplicationController
+
+before_action :require_login, only: [:create]
+
+def create
+  @post = Post.find(params[:post_id])
+  @comment = @post.comments.new(comment_params)
+  @comment.user = current_user
+  if @comment.save
+    flash[:success] = "Thank you!"
+    redirect_to @post
+  else
+    flash[:danger] = "That didn't save."
+    redirect_to :back
+  end
+end
+
+
+private
+
+def comment_params
+  params.require(:comment).permit(:body)
+end
+
+
+
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,25 +1,6 @@
 class Comment < ApplicationRecord
 
-before_action :require_login, only: [:create]
-
-def create
-  @post = Post.find(params[:post_id])
-  @comment = @post.rejoiners.new(comment_params)
-  @comment.user = current_user
-  if @comment.save
-    flash[:success] = "Consider it rejoined!"
-    redirect_to @post
-  else
-    flash[:danger] = "That didn't save."
-    redirect_to :back
-  end
-end
-
-
-private
-
-def comment_params
-  params.require(:comment).permit(:body)
-end
+    belongs_to :user
+    belongs_to :post
 
 end
